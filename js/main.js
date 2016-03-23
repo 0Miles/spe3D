0d4bb4529f37753a420e0fc9ecc1cfb0e99a6c76
Miles <swrev5741@gmail.com>
2016-03-23 16:44:50 +0800

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var playt = false;
 
-$(window).load(function() {
+/*! Copyright 2011, Ben Lin (http://dreamerslab.com/)
+* Licensed under the MIT License (LICENSE.txt).
+*
+* Version: 1.0.3
+*
+* Requires: jQuery 1.2.3+
+*/
+(function(a){a.preload=function(){var d=Object.prototype.toString.call(arguments[0])==="[object Array]"?arguments[0]:arguments;
+var c=[];var b=d.length;for(;b--;){c.push(a("<img />").attr("src",d[b]));}};})(jQuery);
 
+$(window).load(function() {
+    $.preload( 'images/c_twitter_h.png',
+        'images/c_insta_h.png',
+        'images/c_youtube_h.png',
+        'images/c_facebook_h.png'
+      );
   // $("body").everyTime("5s",function(){
   //   if($(".logo").css("margin-left") == "0px"){
   //     $(".logo").css("margin-left", "-100%");
